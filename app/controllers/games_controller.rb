6fa848e5c91f4cb5747d5af288c6b010e4691507
Fany Servier <fany.servier@gmail.com>
2023-05-18 10:11:47 +0200

    rails-longest-word-game update

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,19 +1,32 @@
+require 'open-uri'
+
 class GamesController < ApplicationController
+  VOWELS = %w(A E I O U Y)
+
   def new
-    # Afficher les lettres au hasard
-    # Définir un array avec l'alphabet
-    alphabet = ('a'..'z').to_a
-    # Renvoyer 10 lettres au hasard
-    @letters = alphabet.sample(10)
-    # Formulaire où l’utilisateur peut taper un mot + bouton d’envoi
+    @letters = Array.new(5) { VOWELS.sample }
+    @letters += Array.new(5) { (('A'..'Z').to_a - VOWELS).sample }
+    @letters.shuffle!
   end
 
   def score
-    # Récupérer les éléments du formulaire
-    @score = params[:word]
-    # Effectuer la requête POST
-    post 'score', params: score
-    # Calculer le score
-    # Afficher le score
+    @letters = params[:letters]
+    @word = (params[:word] || '').upcase
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
+  end
+
+  private
+
+  def included?(word, letters)
+    return false if word.nil? || letters.nil?
+
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
+  def english_word?(word)
+    response = URI.open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
   end
 end
